2da457c2e9593fe36db860ee670329cb4d1dfbb7
vbhv007 <bhardwajvbhv@gmail.com>
2017-12-24 19:08:42 +0530

    Some changes made

---
+++ p5-projects/sketch.js
@@ -0,0 +1,19 @@
+
+
+function setup() {
+    createCanvas(600,400);
+    background(0);
+}
+
+function draw() {
+  stroke(145);
+  fill(255);
+  rect(100,100,50,50);
+  stroke(145);
+  fill(50);
+  ellipse(150,150,50,50);
+  stroke(145);
+  fill(150);
+  line(200,250,50);
+
+}
