d69511b09d4d2bfc8eaee2033ac2075ce83e5702
vbhv007 <bhardwajvbhv@gmail.com>
2018-02-01 10:23:35 +0530

    basic cartoon figure using shapes and colors.

--- p5-projects/sketch.js
+++ p5-projects/sketch.js
@@ -1,19 +1,24 @@
-
-
 function setup() {
-    createCanvas(600,400);
-    background(0);
+    createCanvas(400,400);
+
 }
 
-function draw() {
-  stroke(145);
-  fill(255);
-  rect(100,100,50,50);
-  stroke(145);
-  fill(50);
-  ellipse(150,150,50,50);
-  stroke(145);
-  fill(150);
-  line(200,250,50);
 
+
+function draw() {
+    background(150);
+    stroke(0);
+    fill(0,0,255);
+    rect(250,150,50,125);
+    fill(255,200,0);
+    ellipse(275,150,75,75);
+    line(250,275,225,325);
+    line(300,275,325,325);
+    fill(0,255,0);
+    ellipse(250,150,15,30);
+    ellipse(300,150,15,30);
+    fill(255,0,0);
+    ellipse(275,175,15,10);
+    line(250,225,200,150);
+    line(300,225,350,150);
 }
